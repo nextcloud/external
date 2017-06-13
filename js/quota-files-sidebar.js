9fb11145e790d1774e14ed07c43c83a220936175
Joas Schilling <coding@schilljs.com>
2017-06-13 11:17:01 +0200

    Fix trashbin with new position
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- js/quota-files-sidebar.js
+++ js/quota-files-sidebar.js
@@ -3,9 +3,10 @@ $(document).ready(function () {
 	var $p = $('<p>'),
 		$quotaLink = $('#external_quota_link'),
 		$quotaName = $('#external_quota_name');
-	$p.addClass('quotatext-additional').text($quotaName.val());
-	$('li#quota').on('click', function(e) {
+	$p.text($quotaName.val());
+	$('li#quota').on('click', function() {
 		OC.redirect($quotaLink.val());
 	});
 	$('li#quota div.quota-container').after($p);
+	$('li.nav-trashbin').attr('style', 'margin-bottom: 36px !important');
 });
