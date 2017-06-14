a59b433f341c35ffdd1e0b178cac394464992a3c
Morris Jobke <hey@morrisjobke.de>
2017-06-13 22:49:18 -0500

    Improve margin for overflow
    Signed-off-by: Morris Jobke <hey@morrisjobke.de>

--- js/quota-files-sidebar.js
+++ js/quota-files-sidebar.js
@@ -9,4 +9,11 @@ $(document).ready(function () {
 	});
 	$('li#quota div.quota-container').after($p);
 	$('li.nav-trashbin').attr('style', 'margin-bottom: 36px !important');
+
+	if ($('li.nav-trashbin').exists()) {
+		$('#app-navigation > ul li:nth-last-child(2)').attr('style', 'margin-bottom: 124px !important');
+	} else {
+		$('#app-navigation > ul li:nth-last-child(1)').attr('style', 'margin-bottom: 80px !important');
+
+	}
 });
