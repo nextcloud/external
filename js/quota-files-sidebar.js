a23c01d5a9570d9f3d1108bf6323284cde9f5f2f
John Molakvoæ <skjnldsv@users.noreply.github.com>
2018-05-03 17:33:45 +0200

    Fix trashbin
    Signed-off-by: John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>

--- js/quota-files-sidebar.js
+++ js/quota-files-sidebar.js
@@ -8,12 +8,4 @@ $(document).ready(function () {
 		OC.redirect($quotaLink.val());
 	});
 	$('li#quota div.quota-container').after($p);
-	var style = '#app-navigation li.nav-trashbin {margin-bottom: 36px !important; }';
-
-	if ($('li.nav-trashbin').exists()) {
-		style += ' #app-navigation > ul li:nth-last-child(2) { margin-bottom: 124px !important; }';
-	} else {
-		style += ' #app-navigation > ul li:nth-last-child(1) { margin-bottom: 80px !important; }';
-	}
-	$('head').append('<style type="text/css">' + style + '</style>');
 });
