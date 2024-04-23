296bb81e55433e8bedca14f75f463ea860400a76
Andy Scherzinger <info@andy-scherzinger.de>
2024-04-23 21:19:17 +0200

    Add SPDX header
    Signed-off-by: Andy Scherzinger <info@andy-scherzinger.de>

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -2,6 +2,14 @@
 
 declare(strict_types=1);
 
+/**
+ * @copyright Copyright (c) 2017 Joas Schilling <coding@schilljs.com>
+ * @license GNU AGPL version 3 or any later version
+ *
+ * SPDX-FileCopyrightText: 2017 Joas Schilling <coding@schilljs.com>
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
 require_once './vendor/autoload.php';
 
 use Nextcloud\CodingStandard\Config;
--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+<!--
+ ~ SPDX-FileCopyrightText: 2017 Joas Schilling <coding@schilljs.com>
+ ~ SPDX-License-Identifier: AGPL-3.0-or-later
+-->
 # Changelog
 All notable changes to this project will be documented in this file.
 
