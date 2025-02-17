da8147fd3fc0eabeac29a0f51feef64b29cf4643
RMM <schissdraeck+github@rmm.li>
2025-02-17 09:02:26 -0500

    fix formatting
    Signed-off-by: RMM <schissdraeck+github@rmm.li>

--- lib/SitesManager.php
+++ lib/SitesManager.php
@@ -103,6 +103,7 @@ class SitesManager {
 	public function getSitesToDisplay() {
 		$sites = $this->getSites();
 		$lang = $this->languageFactory->findLanguage();
+		$locale = $this->languageFactory->findLocale();
 		$device = $this->getDeviceFromUserAgent();
 
 		$user = $this->userSession->getUser();
@@ -138,8 +139,8 @@ class SitesManager {
 			}
 
 			$site['url'] = str_replace(
-			    ['{email}', '{uid}', '{displayname}', '{jwt}', '{groups}', '{language}'],
-			    array_map('rawurlencode', [$email, $uid, $displayName, $jwt, implode(',', $groups), $lang]),
+				['{email}', '{uid}', '{displayname}', '{jwt}', '{groups}', '{language}'],
+				array_map('rawurlencode', [$email, $uid, $displayName, $jwt, implode(',', $groups), $lang]),
 				$site['url']
 			);
 
